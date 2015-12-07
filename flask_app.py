1f2e7b2dd530d8e1de5475a59f64c98db9423de7
kjflyback <tangxb@rising.com.cn>
2015-12-07 14:24:03 +0800

    配置本地执行的环境

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,9 @@
-
+import os
 from dailywork import app as application
+print application.config
+basedir = os.path.abspath('')
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "app.db")
+application.config['SQLALCHEMY_MIGRATE_REPO'] = os.path.join(basedir, 'db_repository')
+
 application.debug = True
 application.run('', 8000)  
