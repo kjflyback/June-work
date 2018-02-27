c48919252efbcaa11a8a06fe5b45b85240eba0bc
kjflyback <tangxb@rising.com.cn>
2018-02-27 22:24:00 +0800

    add login

---
+++ initLeanCloud.js
@@ -0,0 +1,8 @@
+// LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
+// https://leancloud.cn/docs/sdk_setup-js.html#初始化
+var APP_ID = 'ARLfDFch9OmH7CnGrAaL4Kiq-gzGzoHsz';
+var APP_KEY = 'uLp30boDaFhNCof4SK5az0n9';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
