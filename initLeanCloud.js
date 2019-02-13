73d865d6c19b1734764250f7cb28d80c93b171b4
kjflyback <tangxb@rising.com.cn>
2019-02-13 13:47:08 +0800

    change to 2019

--- initLeanCloud.js
+++ initLeanCloud.js
@@ -1,7 +1,10 @@
 // LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
 // https://leancloud.cn/docs/sdk_setup-js.html#初始化
-var APP_ID = 'ARLfDFch9OmH7CnGrAaL4Kiq-gzGzoHsz';
-var APP_KEY = 'uLp30boDaFhNCof4SK5az0n9';
+// var APP_ID = 'ARLfDFch9OmH7CnGrAaL4Kiq-gzGzoHsz';
+// var APP_KEY = 'uLp30boDaFhNCof4SK5az0n9';
+
+var APP_ID = 'WgBTgLgggicSSF1XP3mKVkBH-gzGzoHsz';
+var APP_KEY = 'zWF8MHyl8D8ziYqPzPrQ5dlu';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
